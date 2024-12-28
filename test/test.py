a2a69a3675b3fbbc749fe24616267e35106189a6
Adam <ubuntu@ubuntu2004.linuxvmimages.local>
2024-12-28 16:03:40 -0500

    update

--- test/test.py
+++ test/test.py
@@ -19,14 +19,14 @@ async def test_project(dut):
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
-    dut.rst_n.value = 1
-    await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 10)
+    dut.rst_n.value = 1
 
     dut._log.info("Test project behavior")
 
     # Set the input values you want to test
-    dut.ui_in.value = 3
+    dut.ui_in.value = 15
     ## dut.uio_in.value = 30
 
     # Wait for one clock cycle to see the output values
